13eff1b8723dd1419794e1b27492915f5ef0852e
Lucas <lucasrs202@gmail.com>
2024-07-29 11:08:46 -0300

    Initial commit

---
+++ src/aula1/collections/Exercicio1.java
@@ -0,0 +1,22 @@
+package aula1.collections;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Exercicio1 {
+
+	public static void main(String[] args) {
+
+		Random r = new Random();
+		ArrayList<Integer> numbers = new ArrayList<Integer>();
+
+		for (int i = 0; i < 50; i++) {
+			numbers.add(r.nextInt(10));
+		}
+		
+		numbers.sort(null);
+		System.out.println(numbers);
+		
+	}
+
+}
